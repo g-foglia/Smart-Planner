63daecadadb73b1c9c235aa8e0c3232c934def27
Gennaro <gennyfoglia71@gmail.com>
2024-01-09 14:18:45 +0100

    prova da pc fisso

--- src/main/java/org/gennisilv/smartplanner/HelloApplication.java
+++ src/main/java/org/gennisilv/smartplanner/HelloApplication.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        //v 1
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
