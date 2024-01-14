0cbb60567583259dda6e5b91aad990b87d48b49b
zzzzilv <s.cafaro7@studenti.unisa.it>
2024-01-14 18:30:35 +0100

    choiceBox priorità impegno

---
+++ src/main/java/org/gennisilv/smartplanner/inserimentoImpegnoController.java
@@ -0,0 +1,61 @@
+package org.gennisilv.smartplanner;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class inserimentoImpegnoController extends barraController implements Initializable {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private Label prioritaLabelID;
+    @FXML
+    private ChoiceBox<String> prioritaID;
+
+    private String[] priorita = {"1", "2", "3", "4", "5"};
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        prioritaID.getItems().addAll(priorita);
+    }
+
+    public void switchTosettimanale(ActionEvent e) throws IOException {
+        super.switchTosettimanale(e);
+    }
+    public void switchToHome (ActionEvent e) throws IOException
+    {
+        super.switchToHome(e);
+    }
+    public void switchToAreaUtente (ActionEvent e) throws IOException
+    {
+        super.switchToAreaUtente(e);
+    }
+    public void switchToListaCalendari (ActionEvent e) throws IOException
+    {
+        super.switchToListaCalendari(e);
+    }
+    public void switchToAggiuntaEvento (ActionEvent e) throws IOException
+    {
+        super.switchToAggiuntaEvento(e);
+    }
+    public void switchToRicerca (ActionEvent e) throws IOException
+    {
+        super.switchToRicerca(e);
+    }
+
+
+}
