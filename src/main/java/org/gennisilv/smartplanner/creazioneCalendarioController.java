77686607f0e0ff37e0fc59eefd556efb1b1e9573
isaboutoftown <vivispillthetea@gmail.com>
2024-01-14 18:04:33 +0100

    Aggiunti creazione calendario e il suo controller

---
+++ src/main/java/org/gennisilv/smartplanner/creazioneCalendarioController.java
@@ -0,0 +1,45 @@
+package org.gennisilv.smartplanner;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class creazioneCalendarioController extends barraController{
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+    @Override
+    public void switchTosettimanale(ActionEvent event) throws IOException {
+        super.switchTosettimanale(event);
+    }
+
+    @Override
+    public void switchToHome(ActionEvent e) throws IOException {
+        super.switchToHome(e);
+    }
+
+    @Override
+    public void switchToAreaUtente(ActionEvent e) throws IOException {
+        super.switchToAreaUtente(e);
+    }
+
+    @Override
+    public void switchToListaCalendari(ActionEvent e) throws IOException {
+        super.switchToListaCalendari(e);
+    }
+
+    @Override
+    public void switchToRicerca(ActionEvent e) throws IOException {
+        super.switchToRicerca(e);
+    }
+    public void switchToAggiuntaEvento (ActionEvent e) throws IOException
+    {
+        super.switchToAggiuntaEvento(e);
+    }
+
+}
