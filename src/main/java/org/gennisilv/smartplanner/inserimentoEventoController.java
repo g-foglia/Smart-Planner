be27b1a31b18f65156a0be87742f94598386ddfb
Gennaro <gennyfoglia71@gmail.com>
2024-01-26 20:23:08 +0100

    aggiunti i controlli sui form per la registrazione e l'inserimento di un evento

--- src/main/java/org/gennisilv/smartplanner/inserimentoEventoController.java
+++ src/main/java/org/gennisilv/smartplanner/inserimentoEventoController.java
@@ -3,24 +3,20 @@ package org.gennisilv.smartplanner;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.gennisilv.smartplanner.data.Calendario;
-import org.gennisilv.smartplanner.data.CalendarioDAO;
-import org.gennisilv.smartplanner.data.Evento;
-import org.gennisilv.smartplanner.data.EventoDAO;
 import org.gennisilv.smartplanner.logic.CalendarioLogic;
 import org.gennisilv.smartplanner.logic.EventoLogic;
-import org.gennisilv.smartplanner.utils.UserHolder;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 
 public class inserimentoEventoController extends barraController implements Initializable {
     private Stage stage;
@@ -33,10 +29,6 @@ public class inserimentoEventoController extends barraController implements Init
     @FXML
     private DatePicker dataEvento;
     @FXML
-    private TextField oraInizio;
-    @FXML
-    private TextField oraFine;
-    @FXML
     private ChoiceBox<String> periodicitaID;
     @FXML
     private ColorPicker colore;
@@ -44,8 +36,18 @@ public class inserimentoEventoController extends barraController implements Init
     private RadioButton notifiche;
     @FXML
     private ComboBox calendari;
+    @FXML
+    private ComboBox oreInizio;
+    @FXML
+    private ComboBox minutiInizio;
+    @FXML
+    private ComboBox oreFine;
+    @FXML
+    private ComboBox minutiFine;
 
-    private String[] periodicita = {"0 - solo 1 volta;", "1 - 1 volta al giorno;", "2 - 1 volta a settimana;", "3 - 1 volta al mese;"};
+    private final String[] periodicita = {"solo 1 volta", "1 volta al giorno", "1 volta a settimana", "1 volta al mese"};
+    private final String[] orari = {"00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"};
+    private final String[] frazioni = {"00","05","10","15","20","25","30","35","40","45","50","55"};
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -57,35 +59,68 @@ public class inserimentoEventoController extends barraController implements Init
 
         calendari.getItems().addAll(nomi);
         periodicitaID.getItems().addAll(periodicita);
+        oreInizio.getItems().addAll(orari);
+        minutiInizio.getItems().addAll(frazioni);
+        oreFine.getItems().addAll(orari);
+        minutiFine.getItems().addAll(frazioni);
     }
 
 
     public void inserisciEvento(ActionEvent e) throws IOException {
         String nome = nomeEvento.getText();
         String descrizione = descrizioneEvento.getText();
-        GregorianCalendar date = new GregorianCalendar(dataEvento.getValue().getYear(),dataEvento.getValue().getMonthValue()-1,dataEvento.getValue().getDayOfMonth());
-        String oraI = oraInizio.getText();
-        String oraF = oraFine.getText();
+        GregorianCalendar dataEvento = new GregorianCalendar(this.dataEvento.getValue().getYear(), this.dataEvento.getValue().getMonthValue()-1, this.dataEvento.getValue().getDayOfMonth());
+        String oraI = oreInizio.getValue() + ":" + minutiInizio.getValue();
+        String oraF = oreFine.getValue() + ":" + minutiFine.getValue();
         Color color = colore.getValue();
         boolean n = notifiche.isSelected();
 
         int periodicita = -1;
         switch (periodicitaID.getValue()){
-            case "0 - solo 1 volta;":
+            case "solo 1 volta":
                 periodicita = 0;
-            case "1 - 1 volta al giorno;":
+            case "1 volta al giorno":
                 periodicita = 1;
-            case "2 - 1 volta a settimana;":
+            case "1 volta a settimana":
                 periodicita = 2;
-            case "3 - 1 volta al mese;":
+            case "1 volta al mese":
                 periodicita = 3;
         }
-        int codiceEvento = EventoLogic.aggiungiEvento(nome,descrizione,date,oraI,oraF,color,n,periodicita);
 
-        Calendario calendario = CalendarioLogic.cercaCalendario((String) calendari.getValue());
-        CalendarioLogic.aggiungiEvento(codiceEvento, calendario.getCodiceCalendario());
+        /*
+            CONTROLLI SUI CAMPI
+         */
 
-        switchTosettimanale(e);
+        if(!checkName(nome))
+            if(lunghezza(descrizione))
+                    if(!date(dataEvento))
+                        if(checkTimeE(oraI,oraF)){
+                            int codiceEvento = EventoLogic.aggiungiEvento(nome,descrizione,dataEvento,oraI,oraF,color,n,periodicita);
+                            Calendario calendario = CalendarioLogic.cercaCalendario((String) calendari.getValue());
+                            CalendarioLogic.aggiungiEvento(codiceEvento, calendario.getCodiceCalendario());
+
+                            switchTosettimanale(e);
+                        }
+                        else{
+                                //orario di fine antecedente alla orario di inizio
+                                Alert alert = new Alert(Alert.AlertType.INFORMATION,"Gli orari di inizio e fine dell'evento non possono essere incrociati");
+                                alert.showAndWait();
+                        }
+                    else{
+                        //data dell'evento appartenente al passato
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION,"Non puoi creare eventi in giorni passati");
+                        alert.showAndWait();
+                    }
+            else{
+                    //descrizione troppo lunga
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION,"La descrizione dell'evento non può superare i 200 caratteri");
+                    alert.showAndWait();
+            }
+        else{
+            //nome che contiene caratteri speciali
+            Alert alert = new Alert(Alert.AlertType.INFORMATION,"Il nome non può contenere caratteri speciali");
+            alert.showAndWait();
+        }
     }
 
     @Override
@@ -115,5 +150,38 @@ public class inserimentoEventoController extends barraController implements Init
     public void switchToAggiuntaEvento (ActionEvent e) throws IOException
     {
         super.switchToAggiuntaEvento(e);
+    }
+
+    private boolean checkName(String nome){
+        return Pattern.compile("[^a-zA-Z0-9]").matcher(nome).find();
+    }
+
+    private boolean lunghezza(String descrizione){
+        if(descrizione.length() > 200)
+            return false;
+        return true;
+    }
+
+    //non funziona
+    private boolean date(GregorianCalendar data){
+        GregorianCalendar dataAttuale = new GregorianCalendar();
+
+        return data.before(dataAttuale);
+    }
+
+    private boolean checkTimeE(String oraI, String oraF){
+        int oraInizio = Integer.parseInt(oraI.substring(0,2));
+        int oraFine = Integer.parseInt(oraF.substring(0,2));
+
+        if(oraFine < oraInizio)
+            return false;
+        else if(oraFine == oraInizio){
+            int minutiInizio = Integer.parseInt(oraI.substring(3));
+            int minutiFine = Integer.parseInt(oraF.substring(3));
+
+            if(minutiFine < minutiInizio)
+                return false;
+        }
+        return true;
     }
 }
--- src/main/java/org/gennisilv/smartplanner/registrazioneController.java
+++ src/main/java/org/gennisilv/smartplanner/registrazioneController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -17,7 +18,10 @@ import org.gennisilv.smartplanner.utils.UserHolder;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class registrazioneController extends barraController{
     private Stage stage;
@@ -56,12 +60,47 @@ public class registrazioneController extends barraController{
         /*
         CONTROLLI SUI CAMPI
          */
-
-
-        UtenteLogic.registrazione(email,password,nome,cognome,nascita,username);
-
-        switchToHome(e);
+        if(!checkName(nome))
+            if(!checkName(cognome))
+                if(date(nascita))
+                    if(checkEmail(email))
+                        if(!formato(username))
+                            if(lunghezza(password)){
+                                UtenteLogic.registrazione(email,password,nome,cognome,nascita,username);
+                                switchToHome(e);
+                            }
+                            else{
+                                //passowrd troppo corta o troppo lunga
+                                Alert alert = new Alert(Alert.AlertType.INFORMATION,"La passqord deve avere una lunghezza compresa tra gli 8 e i 30 caratteri");
+                                alert.showAndWait();
+                            }
+                        else{
+                            //formato username illegale
+                            Alert alert = new Alert(Alert.AlertType.INFORMATION,"L'username non può contenere caratteri speciali");
+                            alert.showAndWait();
+                        }
+                    else{
+                        //email in formato errato
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION,"Inserisci una mail valida");
+                        alert.showAndWait();
+                    }
+                else{
+                    //utente troppo giovane
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION,"Per utilizzare l'applicazione devi avere almeno 16 anni");
+                    alert.showAndWait();
+                }
+            else{
+                //cognome che contiene numeri o caratteri speciali
+                Alert alert = new Alert(Alert.AlertType.INFORMATION,"Il cognome non può contenere numeri o caratteri speciali");
+                alert.showAndWait();
+            }
+        else{
+            //nome che contiene numeri o caratteri speciali
+            Alert alert = new Alert(Alert.AlertType.INFORMATION,"Il nome non può contenere numeri o caratteri speciali");
+            alert.showAndWait();
+        }
     }
+
     public void switchToHome (ActionEvent e) throws IOException
     {
         Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
@@ -77,5 +116,33 @@ public class registrazioneController extends barraController{
         scene=new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    private boolean checkName(String nome){
+        return Pattern.compile("[^a-zA-Z]").matcher(nome).find();
+    }
+
+    private boolean date(GregorianCalendar nascita){
+        GregorianCalendar dataAttuale = new GregorianCalendar();
+        dataAttuale.add(Calendar.YEAR, -16);
+
+        return nascita.before(dataAttuale);
+    }
+
+    private boolean checkEmail(String email){
+        String regex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    private boolean formato(String username){
+        return Pattern.compile("[^a-zA-Z0-9]").matcher(username).find();
+    }
+
+    private boolean lunghezza(String password){
+        if(password.length()<8 || password.length()>30)
+            return false;
+        return true;
     }
 }
