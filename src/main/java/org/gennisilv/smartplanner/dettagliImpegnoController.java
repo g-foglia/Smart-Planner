f436b9621228e337c61db24abdb7aec0b8aa0ad7
Gennaro <gennyfoglia71@gmail.com>
2024-02-15 18:30:03 +0100

    Ho implementato il design pattern observer

--- src/main/java/org/gennisilv/smartplanner/dettagliImpegnoController.java
+++ src/main/java/org/gennisilv/smartplanner/dettagliImpegnoController.java
@@ -2,8 +2,13 @@ package org.gennisilv.smartplanner;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import org.gennisilv.smartplanner.data.entity.Impegno;
 import org.gennisilv.smartplanner.logic.ListaLogic;
 import org.gennisilv.smartplanner.utils.DataHolder;
@@ -13,6 +18,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class dettagliImpegnoController extends barraController implements Initializable {
+    private Stage stage;
+    private Scene scene;
     @FXML
     private Label nome;
     @FXML
@@ -27,6 +34,14 @@ public class dettagliImpegnoController extends barraController implements Initia
         nome.setText(nome.getText() + " " + impegno.getNomeImpegno());
         durata.setText(durata.getText() + " " + impegno.getDurataImpegno());
         priorita.setText(priorita.getText() + " " + impegno.getPrioritaImpegno());
+    }
+
+    public void switchToLista(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("lista.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @Override
